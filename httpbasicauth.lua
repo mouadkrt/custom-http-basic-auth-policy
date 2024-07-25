052a9a069be45c5e0d144fb1f74bc5311c328a18
Sentissi Mouad <m.sentissi@gmail.com>
2024-07-25 13:39:05 +0100

    removed the use og ngx.ctx

--- httpbasicauth.lua
+++ httpbasicauth.lua
@@ -1,28 +1,29 @@
 -- local setmetatable = setmetatable
 
-local _M = require('apicast.policy').new('httpbasicauth', '0.1')
+local policy = require('apicast.policy')
+local _M = policy.new('httpbasicauth', '0.1')
 -- local mt = { __index = _M }
 
 local new = _M.new
 
 function _M.new(config)
-	-- ngx.ctx.auths_configured = config.http_basic_users
-	-- ngx.log(ngx.INFO, '_M.new(config) : auths_configured : ', config.http_basic_users)
-	ngx.ctx.user_authenticated = false
+        -- ngx.ctx.auths_configured = config.http_basic_users
+        -- ngx.log(ngx.INFO, '_M.new(config) : auths_configured : ', config.http_basic_users)
+        -- ngx.ctx.user_authenticated = false
 --return setmetatable({}, mt)
 
 -- Patch 25/07/2024 for case 03879019
-	local self = new(config)
+        local self = new(config)
 
-	if config then
-		self.httpbasicauth = config.http_basic_users
-		ngx.log(ngx.INFO, '_M.new(config) : self.httpbasicauth = ', config.http_basic_users)
-	else
-		self.httpbasicauth = nil
-		ngx.log(ngx.WARN, '_M.new(config) : config ~FALSE')
-	end
+        if config then
+                self.httpbasicauth = config.http_basic_users
+                ngx.log(ngx.INFO, '_M.new(config) : self.httpbasicauth = ', config.http_basic_users)
+        else
+                self.httpbasicauth = nil
+                ngx.log(ngx.WARN, '_M.new(config) : config ~FALSE')
+        end
 
-	return self
+        return self
 end
 
 function mysplit (inputstr, sep)
@@ -30,7 +31,7 @@ function mysplit (inputstr, sep)
            sep = "%s"
         end
         local t={}
-        for str in string.gmatch(inputstr, "([^"..sep.."]+)") do 
+        for str in string.gmatch(inputstr, "([^"..sep.."]+)") do
            table.insert(t, str)
         end
         return t
@@ -64,11 +65,14 @@ function _M:rewrite()
                                 ngx.log(ngx.INFO,'auth_configured_split[2]: ',auth_configured_split[2])
                                 ngx.log(ngx.INFO, 'type of auth_provided_split[1], auth_configured_split[1], auth_provided_split[2], auth_configured_split[2] are ', type(auth_provided_split[1]), ', ', type(auth_configured_split[1]), ', ', type(auth_provided_split[2]), ', ', type(auth_configured_split[2]))
                         if auth_provided_split[1] == auth_configured_split[1] and auth_provided_split[2] == auth_configured_split[2] then
-                                ngx.ctx.user_authenticated = true
+                                --ngx.ctx.user_authenticated = true
                                 ngx.log(ngx.INFO, 'FOUND A MATCH for auth_configured: ', auth_configured)
                                 break
                         else
                                 ngx.log(ngx.NOTICE, 'NO MATCH for auth_configured: ', auth_configured)
+								ngx.status = 403
+								ngx.log(ngx.ERR, "Sending back HTTP 403 : HTTP Basic auth credentials does NOT match")
+								ngx.say("HTTP Basic auth credentials does NOT match")
                         end
                 end
         else
@@ -82,19 +86,19 @@ function _M:rewrite()
 end
 
 
-function _M:access()
+--function _M:access()
   -- ability to deny the request before it is sent upstream
   -- ngx.say('HTTP Basic auth credentilas does NOT match')
-  
-        if not ngx.ctx.user_authenticated then 
-                ngx.status = 403
-                ngx.log(ngx.ERR, "_M:access() : user_authenticated KO. Sending back HTTP 403 : HTTP Basic auth credentials does NOT match")
-                ngx.say("HTTP Basic auth credentials does NOT match")
-        else
-                ngx.log(ngx.INFO, "_M:access() : user_authenticated OK")
-        end
- 
+
+        --if not ngx.ctx.user_authenticated then
+        --        ngx.status = 403
+        --        ngx.log(ngx.ERR, "_M:access() : user_authenticated KO. Sending back HTTP 403 : HTTP Basic auth credentials does NOT match")
+        --        ngx.say("HTTP Basic auth credentials does NOT match")
+        --else
+        --        ngx.log(ngx.INFO, "_M:access() : user_authenticated OK")
+        --end
+
   -- ngx.exit(ngx.HTTP_FORBIDDEN)
-end
+--end
 
 return _M
